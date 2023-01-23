08dd0237136f20f81b0a952c64e6c0c7ce48b96b
d2Sokol <d2.sokolowski@gmail.com>
2023-01-23 15:03:24 +0100

    Initial Commit

---
+++ Source/PROJECT_HOPE/PROJECT_HOPEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PROJECT_HOPEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_HOPE_API APROJECT_HOPEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
